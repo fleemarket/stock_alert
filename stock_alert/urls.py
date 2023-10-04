f1b9aca8bd0652d1e9cf9cc9933e0117b422ea7b
fleemarket <questional777@gmail.com>
2023-10-04 17:25:21 +0900

    홈페이지 설정중

--- stock_alert/urls.py
+++ stock_alert/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('articles.urls')),
+    # path('', include('articles.urls')),
     path('admin/', admin.site.urls),
+    path('/', include('home.urls')),  
 ]
